b5e5bfea81a7d5180fe52f56210359680951679f
orovio <hi@orov.io>
2020-09-17 15:41:32 +0200

    Fire port

--- main.go
+++ main.go
@@ -3,18 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
+	port := "8080"
 
 	// Starts a new Gin instance with no middle-ware
 	r := gin.New()
